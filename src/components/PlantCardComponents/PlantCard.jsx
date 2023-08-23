850fec8bea8410b116916655bad611db868c7a93
Payton Kentch <frankrlystan@gmail.com>
2023-08-23 11:13:47 -0600

    hotfix pt3 -- delete plant -- finished

--- src/components/PlantCardComponents/PlantCard.jsx
+++ src/components/PlantCardComponents/PlantCard.jsx
@@ -1,21 +1,54 @@
 import PlusButton from './PlusButton'
 import MinusButton from './MinusButton'
+import axios from 'axios'
+import { useState } from 'react'
 
 const PlantCard = ({ name, counts, type, img, increment, decrement }) => {
   let count = counts[`${name}`]
+  const [status, setStatus] = useState('alive')
 
-  return (
-    <div className='test-card'>
-      <h1>Name: {name}</h1>
-      <h2>Type: {type}</h2>
-      <img className='PlantImage' src={`${img}`} />
-      <p>Count: {count}</p>
-      <div>
-        <PlusButton name={name} increment={increment} counts={counts} />
-        <MinusButton name={name} decrement={decrement} />
+  if (status === 'alive')
+    return (
+      <div
+        className='test-card'
+        style={{ backgroundColor: 'rgb(215, 255, 230)' }}
+      >
+        <h1>Name: {name}</h1>
+        <h2>Type: {type}</h2>
+        <img className='PlantImage' src={`${img}`} />
+        <p>Count: {count}</p>
+        <div>
+          <PlusButton name={name} increment={increment} counts={counts} />
+          <MinusButton name={name} decrement={decrement} />
+        </div>
+        <button
+          onClick={async () => {
+            await axios.post('/api/count/delete', { name })
+            setStatus('dead')
+          }}
+        >
+          Delete
+        </button>
       </div>
-    </div>
-  )
+    )
+  else
+    return (
+      <div
+        className='test-card'
+        style={{ backgroundColor: 'rgb(255, 215, 215)' }}
+      >
+        <h1>Deleted</h1>
+        <p>{status}</p>
+        <button
+          onClick={async () => {
+            await axios.post('/api/users/newplant', { name, count })
+            setStatus('alive')
+          }}
+        >
+          undo
+        </button>
+      </div>
+    )
 }
 
 export default PlantCard
